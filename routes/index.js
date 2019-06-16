df71962f900162e1b6f6ea3cbddcce337664d003
anish25 <anishgandhi25@gmail.com>
2019-06-16 19:45:24 -0400

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
+// File Name:-index.js
+// Author Name:-Anish Gandhi
+// Website Name:-Portfolio 
+// Description:-comnatins all the routes to different pages in the appliaction
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
