b7ff0cb18f069b312123dc70000ef01dd48ca9ee
anish25 <anishgandhi25@gmail.com>
2019-06-16 20:33:33 -0400

    last final commit

---
+++ README.md
@@ -0,0 +1,13 @@
+References 
+
+Design template used from Bootstrap free templetes
+
+Bootstrap. (n.d.). Creative - One Page Bootstrap Theme. Retrieved from https://startbootstrap.com/themes/creative/
+
+Code References 
+
+Basic routing. (n.d.). Retrieved from https://expressjs.com/en/starter/basic-routing.html
+
+Express Tutorial Part 2: Creating a skeleton website. (n.d.). Retrieved from https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/skeleton_website
+
+George Ornbo. (n.d.). Creating a basic site with Node.js and Express. Retrieved from https://shapeshed.com/creating-a-basic-site-with-node-and-express/
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,37 @@
 // File Name:-index.js
 // Author Name:-Anish Gandhi
 // Website Name:-Portfolio 
-// Description:-comnatins all the routes to different pages in the appliaction
+// Description:-contains all the routes to different pages in the appliaction
 
 var express = require('express');
 var router = express.Router();
 
+const app = express();
+app.set('view engine', 'pug');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+app.get('/', (req, res) => {
+  res.render('About')
+})
+
+app.get('/', (req, res) => {
+  res.render('Services')
+})
+
+app.get('/', (req, res) => {
+  res.render('projects')
+})
+
+app.get('/', (req, res) => {
+  res.render('contact_us')
+})
 
 module.exports = router;
